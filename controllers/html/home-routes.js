ebca04f10b4ea1b4b471ca27957310149261f826
nickporjonathan <nickporjonathan@gmail.com>
2022-04-25 21:43:19 -0400

    home routes done

--- controllers/html/home-routes.js
+++ controllers/html/home-routes.js
@@ -74,3 +74,14 @@ router.get("/post/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
